fa92b5e367800798ddb9f62d791fafb274b62ab3
Edyta Danilkiewicz <edyta.danilkiewicz@gmail.com>
2018-08-23 18:05:25 +0200

    Add tooltips

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,8 @@ gulp.task('js', function() {
   return gulp.src([
     'node_modules/bootstrap/dist/js/bootstrap.min.js',
     'node_modules/jquery/dist/jquery.min.js',
-    'node_modules/popper.js/dist/popper.js'
+    'node_modules/popper.js/dist/umd/popper.js',
+    'js/main.js'
   ])
   .pipe(gulp.dest("src/js"))
   .pipe(browserSync.stream());
